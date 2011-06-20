483f0e9bf88f9d51a3b4e2fca673ee6af537ccce
Diego Rubin <rubin.diego@gmail.com>
2011-06-20 17:49:24 -0300

    Dump completo do banco.

--- db/dump.sql
+++ db/dump.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 5.1.56, for slackware-linux-gnu (i486)
 --
--- Host: localhost    Database: TrabalhoFinal
+-- Host: localhost    Database: Presenca
 -- ------------------------------------------------------
--- Server version   5.1.56
+-- Server version	5.1.56
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -15,9 +15,110 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-DROP DATABASE IF EXISTS `Presenca`;
-CREATE DATABASE `Presenca`;
-USE `Presenca`;
+--
+-- Table structure for table `Aluno`
+--
+
+DROP TABLE IF EXISTS `Aluno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Aluno` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(254) NOT NULL,
+  `anoIngresso` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Aluno`
+--
+
+LOCK TABLES `Aluno` WRITE;
+/*!40000 ALTER TABLE `Aluno` DISABLE KEYS */;
+INSERT INTO `Aluno` VALUES (3,'Rafael Rubin','2007'),(2,'Diego Rubin','2007');
+/*!40000 ALTER TABLE `Aluno` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `AlunoTurma`
+--
+
+DROP TABLE IF EXISTS `AlunoTurma`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `AlunoTurma` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idAluno` int(11) NOT NULL,
+  `idTurma` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `AlunoTurma`
+--
+
+LOCK TABLES `AlunoTurma` WRITE;
+/*!40000 ALTER TABLE `AlunoTurma` DISABLE KEYS */;
+INSERT INTO `AlunoTurma` VALUES (1,3,2),(2,2,2);
+/*!40000 ALTER TABLE `AlunoTurma` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Disciplina`
+--
+
+DROP TABLE IF EXISTS `Disciplina`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Disciplina` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(254) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Disciplina`
+--
+
+LOCK TABLES `Disciplina` WRITE;
+/*!40000 ALTER TABLE `Disciplina` DISABLE KEYS */;
+INSERT INTO `Disciplina` VALUES (1,'Matemática'),(2,'Fisíca'),(3,'Geografia'),(4,'Matematica'),(5,'Fisica'),(6,'Geografia');
+/*!40000 ALTER TABLE `Disciplina` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Endereco`
+--
+
+DROP TABLE IF EXISTS `Endereco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Endereco` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `logradouro` varchar(254) DEFAULT NULL,
+  `cep` varchar(10) DEFAULT NULL,
+  `numero` varchar(10) DEFAULT NULL,
+  `bairro` varchar(100) DEFAULT NULL,
+  `municipio` varchar(100) DEFAULT NULL,
+  `uf` varchar(2) DEFAULT NULL,
+  `classeObjeto` varchar(10) DEFAULT NULL,
+  `idObjeto` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Endereco`
+--
+
+LOCK TABLES `Endereco` WRITE;
+/*!40000 ALTER TABLE `Endereco` DISABLE KEYS */;
+INSERT INTO `Endereco` VALUES (1,'','','','','','','Aluno',1),(2,'Avenida 34-A','13500-000','201','Vila Alemã','Rio Claro','SP','Aluno',2),(3,'','','','','','','Professor',1),(4,'','','','Rua do Zé','','','Professor',2),(5,'Avenida 64-A','13506-057','141','Jardim Amércia','Rio Claro','SP','Aluno',3);
+/*!40000 ALTER TABLE `Endereco` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `Professor`
@@ -27,10 +128,12 @@ DROP TABLE IF EXISTS `Professor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `Professor` (
-      `id` int(11) NOT NULL AUTO_INCREMENT,
-      `nome` varchar(254) NOT NULL,
-      PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(254) NOT NULL,
+  `formacao` varchar(254) NOT NULL,
+  `anoConclusao` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +142,35 @@ CREATE TABLE `Professor` (
 
 LOCK TABLES `Professor` WRITE;
 /*!40000 ALTER TABLE `Professor` DISABLE KEYS */;
+INSERT INTO `Professor` VALUES (2,'Morador da rua do zé','','');
 /*!40000 ALTER TABLE `Professor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Turma`
+--
+
+DROP TABLE IF EXISTS `Turma`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Turma` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `disciplinaId` int(11) NOT NULL,
+  `professorId` int(11) NOT NULL,
+  `semestre` varchar(1) NOT NULL,
+  `ano` varchar(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Turma`
+--
+
+LOCK TABLES `Turma` WRITE;
+/*!40000 ALTER TABLE `Turma` DISABLE KEYS */;
+INSERT INTO `Turma` VALUES (2,3,2,'2','2011');
+/*!40000 ALTER TABLE `Turma` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -51,5 +182,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2011-06-06 13:45:51
-
+-- Dump completed on 2011-06-20 17:48:54
